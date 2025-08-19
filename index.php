a954a982d6a9eae215af4a222181bcbcb6bf67b1
bahanley <benhanley321@gmail.com>
2025-08-19 22:49:21 +0100

    mvc

---
+++ index.php
@@ -0,0 +1,56 @@
+<?php
+
+// Autoload classes using Composer (assuming you have a PSR-4 autoloader set up)
+require_once 'vendor/autoload.php';
+// Namespace declaration
+
+use App\Controllers;
+
+$request_uri = $_SERVER['REQUEST_URI'];
+$base_uri = str_replace('/index.php', '', $_SERVER['SCRIPT_NAME']);
+$uri = substr($request_uri, strlen($base_uri));
+$prefix = '/st-marys';
+if (strpos($uri, $prefix) === 0) {
+    $uri = substr($uri, strlen($prefix));
+}
+
+// use App\Controllers\HomeController;
+// Route to the appropriate controller based on the URI
+switch ($uri) {
+	case '/':
+		$controller = new Controllers\HomeController();
+		$controller->index();
+		break;
+	case '/index.php':
+		$controller = new Controllers\HomeController();
+		$controller->index();
+		break;
+	case '/index':
+		$controller = new Controllers\HomeController();
+		$controller->index();
+		break;
+	case '/about-us':
+		$controller = new Controllers\ErrorController();
+		$controller->index();
+		break;
+	case '/media':
+		$controller = new Controllers\ErrorController();
+		$controller->index();
+		break;
+	case '/events':
+		$controller = new Controllers\ErrorController();
+		$controller->index();
+		break;
+	case '/contact-us':
+		$controller = new Controllers\ErrorController();
+		$controller->index();
+		break;
+	case '/donate':
+		$controller = new Controllers\ErrorController();
+		$controller->index();
+		break;
+	default:
+		http_response_code(404);
+		echo '404 Not Found';
+		break;
+}
---
+++ src/views/Shared/Home.php
@@ -0,0 +1,41 @@
+<?php
+require_once 'Header.php';
+?>
+
+<div class="bg-indigo-900 justify-center mx-6 my-6 rounded-xl sm:py-32">
+  <div class="mx-auto max-w-7xl px-6 lg:px-8">
+    <div class="mx-auto max-w-4xl text-center">
+      <h3 class="text-base font-semibold leading-7 text-indigo-300">St Marys Wollaton Park:</h3>
+      <p class="mt-2 text-4xl font-bold tracking-tight text-gray-100 sm:text-5xl">Evangelical Anglican Church</p>
+    </div>
+    <p class="mx-auto mt-6 max-w-2xl text-center text-lg leading-8 text-gray-100">Select one of the options below</p>
+    </div>
+    <div class="mt-10 grid-cols-2 grid gap-4">
+		<a href="/st-marys/about-us">
+			<div class="rounded-3xl mx-6 p-8 ring-1 ring-gray-200">
+				<h3 class="text-lg font-semibold leading-8 text-gray-100">About us</h3>
+				<p class="mt-4 text-sm leading-6 text-gray-300">Find about our family and history.</p>
+			</div>
+		</a>
+		<a href="/st-marys/media">
+			<div class="rounded-3xl mx-6 p-8 ring-1 ring-gray-200">
+				<h3 class="text-lg font-semibold leading-8 text-gray-100">Media</h3>
+				<p class="mt-4 text-sm leading-6 text-gray-300">Explore our most recent services.</p>
+			</div>
+		</a>
+		<a href="/st-marys/events">
+			<div class="rounded-3xl mx-6 p-8 ring-1 ring-gray-200">
+				<h3 class="text-lg font-semibold leading-8 text-gray-100">Events</h3>
+				<p class="mt-4 text-sm leading-6 text-gray-300">See all our upcoming events.</p>
+			</div>
+		</a>
+		<a href="/st-marys/donate">
+			<div class="rounded-3xl mx-6 p-8 ring-1 ring-gray-200">
+				<h3 class="text-lg font-semibold leading-8 text-gray-100">Donate</h3>
+				<p class="mt-4 text-sm leading-6 text-gray-300">Support us financially.</p>
+			</div>
+		</a>
+	  </div>
+    </div>
+  </div>
+</div>
