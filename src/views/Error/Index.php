e5ea656d13f11ef01ae1c6f257aa0f237e29ef6d
bahanley <benhanley321@gmail.com>
2025-08-20 15:13:19 +0100

    better 404 page

--- src/views/Error/Index.php
+++ src/views/Error/Index.php
@@ -5,7 +5,11 @@ require_once 'src/views/Shared/Header.php';
 <div class="bg-indigo-900 justify-center mx-6 my-6 rounded-xl sm:py-32">
   <div class="mx-auto max-w-7xl px-6 lg:px-8">
     <div class="mx-auto max-w-4xl text-center">
-      <h5 class="text-base font-semibold leading-7 text-indigo-300">Error 404: Page Not Found</h5>
+      <p class="mt-2 text-4xl font-bold tracking-tight text-gray-100 sm:text-5xl">
+        Error 404: Couldn't find page
+      </p>
     </div>
+    <a href="https://twowaystolive.com/" class="mx-auto mt-6 underline max-w-2xl text-center text-lg leading-8 text-gray-100">Find Christ instead :)</a>
+    
   </div>
 </div>
