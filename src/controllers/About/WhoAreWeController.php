954502ea57997105bb198c0b64af1623c76211c4
bahanley <benhanley321@gmail.com>
2025-08-20 18:51:16 +0100

    who are we controller

--- src/controllers/About/WhoAreWeController.php
+++ src/controllers/About/WhoAreWeController.php
@@ -1,12 +1,12 @@
 <?php
-namespace App\Controllers;
+namespace App\Controllers\About;
 
 class WhoAreWeController {
 
 	public function __construct() {}
 
 	public function index() {
-        require_once('src/views/Error/Index.php');
+        require_once('src/views/About/WhoAreWe.php');
     }
 
 }
