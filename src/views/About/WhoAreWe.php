67510c7a4c676511cce955d28695aef47d9d3074
bahanley <benhanley321@gmail.com>
2025-08-20 18:52:39 +0100

    who are we page

---
+++ src/views/About/WhoAreWe.php
@@ -0,0 +1,81 @@
+<?php
+require_once 'src/views/Shared/Header.php';
+?>
+
+  <div class="py-8 px-4 mx-auto max-w-screen-xl lg:py-16 lg:px-6 ">
+      <div class="mx-auto max-w-screen-sm text-center mb-8 lg:mb-16">
+          <h3 class="text-base font-semibold leading-7 text-indigo-300">St Mary's Wollaton Park:</h3>
+          <p class="mt-2 text-4xl font-bold tracking-tight text-gray-100 sm:text-5xl">Who we are</p>
+       </div> 
+
+      <div class="grid gap-8 mb-6 lg:mb-16 md:grid-cols-2">
+          <div class="items-center bg-gray-50 rounded-lg shadow sm:flex dark:bg-indigo-900 dark:border-gray-700">
+              <a href="#">
+                  <img class="w-full rounded-lg sm:rounded-none sm:rounded-l-lg" src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/avatars/bonnie-green.png" alt="Bonnie Avatar">
+              </a>
+              <div class="p-5">
+                  <h3 class="text-xl font-bold tracking-tight text-gray-900 dark:text-white">
+                      <a href="#">Rob</a>
+                  </h3>
+                  <span class="text-blue-200 dark:text-white">Vicar</span>
+                  <p class="mt-3 mb-4 font-light text-blue-200 dark:text-white">Bonnie drives the technical strategy of the flowbite platform and brand.</p>
+                  <a href="/st-marys/contact-us" 
+                      class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-2 py-2 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                      Contact us
+                  </a>
+              </div>
+          </div> 
+          <div class="items-center bg-gray-50 rounded-lg shadow sm:flex dark:bg-indigo-900 dark:border-gray-700">
+              <a href="#">
+                  <img class="w-full rounded-lg sm:rounded-none sm:rounded-l-lg" src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/avatars/jese-leos.png" alt="Jese Avatar">
+              </a>
+              <div class="p-5">
+                  <h3 class="text-xl font-bold tracking-tight text-gray-900 dark:text-white">
+                      <a href="#">Grace</a>
+                  </h3>
+                  <span class="text-blue-200 dark:text-white">Administrator</span>
+                  <p class="mt-3 mb-4 font-light text-blue-200 dark:text-white">Jese drives the technical strategy of the flowbite platform and brand.</p>
+                  <a href="/st-marys/contact-us" 
+                      class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-2 py-2 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                      Contact us
+                  </a>
+              </div>
+          </div> 
+          <div class="items-center bg-gray-50 rounded-lg shadow sm:flex dark:bg-indigo-900 dark:border-gray-700">
+              <a href="#">
+                  <img class="w-full rounded-lg sm:rounded-none sm:rounded-l-lg" src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/avatars/michael-gouch.png" alt="Michael Avatar">
+              </a>
+              <div class="p-5">
+                  <h3 class="text-xl font-bold tracking-tight text-gray-900 dark:text-white">
+                      <a href="#">David</a>
+                  </h3>
+                  <span class="text-blue-200 dark:text-white">Warden</span>
+                  <p class="mt-3 mb-4 font-light text-blue-200 dark:text-white">Michael drives the technical strategy of the flowbite platform and brand.</p>
+                  <a href="/st-marys/contact-us" 
+                      class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-2 py-2 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                      Contact us
+                  </a>
+              </div>
+          </div> 
+          <div class="items-center bg-gray-50 rounded-lg shadow sm:flex dark:bg-indigo-900 dark:border-gray-700">
+              <a href="#">
+                  <img class="w-full rounded-lg sm:rounded-none sm:rounded-l-lg" src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/avatars/sofia-mcguire.png" alt="Sofia Avatar">
+              </a>
+              <div class="p-5">
+                  <h3 class="text-xl font-bold tracking-tight text-gray-900 dark:text-white">
+                      <a href="#">Sean</a>
+                  </h3>
+                  <span class="text-blue-200 dark:text-white">UCCF Mission Partner</span>
+                  <p class="mt-3 mb-4 font-light text-blue-200 dark:text-white">Lana drives the technical strategy of the flowbite platform and brand.</p>
+                  <a href="/st-marys/contact-us" 
+                      class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-2 py-2 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                      Find out more
+                  </a>
+              </div>
+          </div>  
+      </div>  
+  </div>
+
+<?php
+require_once 'src/views/Shared/Footer.php';
+?>
