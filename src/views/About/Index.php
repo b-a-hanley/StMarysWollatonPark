a94e6dce3d5989db9a9eb90cfbc84a5f743b6a94
bahanley <benhanley321@gmail.com>
2025-08-20 15:13:32 +0100

    basic about us

---
+++ src/views/About/Index.php
@@ -0,0 +1,33 @@
+<?php
+require_once 'src/views/Shared/Header.php';
+?>
+
+<div class="bg-indigo-900 justify-center mx-6 my-6 rounded-xl sm:py-32">
+  <div class="mx-auto max-w-7xl px-6 lg:px-8">
+    <div class="mx-auto max-w-4xl text-center">
+      <h3 class="text-base font-semibold leading-7 text-indigo-300">St Mary's Wollaton Park:</h3>
+      <p class="mt-2 text-4xl font-bold tracking-tight text-gray-100 sm:text-5xl">Our Story</p>
+    </div>
+    <p class="mx-auto mt-6 max-w-2xl text-center text-lg leading-8 text-gray-100"> When Wollaton Park was sold by the Middletons to Nottingham Corporation, a garden suburb was developed in this area.</p>
+	<p class="mx-auto mt-6 max-w-2xl text-center text-lg leading-8 text-gray-100"> The corporation also acquired land that is now the Lenton Abbey estate, centered around a large house still known as Lenton Abbey, which is currently owned by the University of Nottingham. This acquisition extended the corporation's land holdings from the Drill Hall at the top of Derby Road, westward towards what became Woodside Road, and as far as Trent Side in North Wilford. The residential development in this area created a demand for new churches. As a result, two churches were built: St Barnabas, serving the new Lenton Abbey estate, and St Mary's in Wollaton Park.</p>
+	<p class="mx-auto mt-6 max-w-2xl text-center text-lg leading-8 text-gray-100"> St Mary's was designed by the Nottingham architect T.C. Howitt between 1937 and 1939, who was also a worshipper at the new church. The design included a church hall and vicarage. ​</p>
+	<p class="mx-auto mt-6 max-w-2xl text-center text-lg leading-8 text-gray-100"> The church was dedicated to St Mary in honour of the lost village of Sutton Passeys which would of been located somewhere in wollaton park. Sutton Passey's church, St Mary the virgin, dated back to 1340 but now is lost with the rest of the village.​</p>
+	</div>
+	<div class="mx-auto max-w-4xl text-center">
+      <p class="mt-2 text-2xl font-bold tracking-tight text-gray-100 sm:text-2xl">Read more</p>
+    </div>
+    <div class="mt-10 grid-cols-2 grid gap-4">
+		<a href="/st-marys/about-us">
+			<div class="rounded-3xl mx-6 p-8 ring-1 ring-gray-200">
+				<h3 class="text-lg font-semibold leading-8 text-gray-100">St Mary's page of the Church History Project</h3>
+			</div>
+		</a>
+		<a href="/st-marys/media">
+			<div class="rounded-3xl mx-6 p-8 ring-1 ring-gray-200">
+				<h3 class="text-lg font-semibold leading-8 text-gray-100"> Les Berry’s “St. Mary’s Church Wollaton Park"</h3>
+			</div>
+		</a>
+	  </div>
+    </div>
+  </div>
+</div>
