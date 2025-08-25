57aa752ce970887e42d237e07040d3b42d505cf5
bahanley <benhanley321@gmail.com>
2025-08-25 22:52:26 +0100

    navigation double drop down and ui colour

--- src/views/Shared/Header.php
+++ src/views/Shared/Header.php
@@ -17,73 +17,89 @@
     <div class="hidden w-full md:block md:w-auto" id="navbar-default">
       <ul class="font-medium flex flex-col p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
         <li>
-          <button id="dropdownAboutUsLink" data-dropdown-toggle="dropdownAboutUs" class="flex items-center justify-between w-full py-2 px-3 text-gray-900 hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 md:w-auto dark:text-white md:dark:hover:text-blue-500 dark:focus:text-white dark:hover:bg-gray-700 md:dark:hover:bg-transparent">About us <svg class="w-2.5 h-2.5 ms-2.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
+          <button id="dropdownAboutUsLink" data-dropdown-toggle="dropdownAboutUs" class="flex items-center justify-between w-full py-2 px-3 text-gray-900 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 md:w-auto dark:text-white md:dark:hover:text-blue-500 dark:focus:text-white dark:hover:bg-gray-700 md:dark:hover:bg-transparent">About us <svg class="w-2.5 h-2.5 ms-2.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
             <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 4 4 4-4"/>
           </svg></button>
           <div id="dropdownAboutUs" class="z-10 hidden font-normal bg-white divide-y divide-gray-100 rounded-lg shadow-sm w-44 dark:bg-[rgb(32,32,80)] dark:divide-gray-600">
               <ul class="py-2 text-sm text-gray-700 dark:text-gray-200" aria-labelledby="dropdownLargeButton">
                 <li>
-                  <a href="/st-marys/our-story" class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Our Story</a>
+                  <a href="/st-marys/our-story" class="block px-4 py-2 md:hover:bg-transparent md:border-0 md:hover:text-blue-500">Our Story</a>
                 </li>
                 <li>
-                  <a href="/st-marys/our-team" class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Our Team</a>
+                  <a href="/st-marys/our-team" class="block px-4 py-2 md:hover:bg-transparent md:border-0 md:hover:text-blue-500">Our Team</a>
                 </li>
                 <li>
-                  <a href="/st-marys/our-beliefs" class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Our Beliefs</a>
+                  <a href="/st-marys/our-beliefs" class="block px-4 py-2 md:hover:bg-transparent md:border-0 md:hover:text-blue-500">Our Beliefs</a>
                 </li>
               </ul>
           </div>
         </li>
         <li>
-          <button id="dropdownWhatsOnLink" data-dropdown-toggle="dropdownWhatsOn" class="flex items-center justify-between w-full py-2 px-3 text-gray-900 hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 md:w-auto dark:text-white md:dark:hover:text-blue-500 dark:focus:text-white dark:hover:bg-gray-700 md:dark:hover:bg-transparent">What's on<svg class="w-2.5 h-2.5 ms-2.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
+          <button id="dropdownWhatsOnLink" data-dropdown-toggle="dropdownWhatsOn" class="flex items-center justify-between w-full py-2 px-3 text-gray-900 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 md:w-auto dark:text-white md:dark:hover:text-blue-500 dark:focus:text-white dark:hover:bg-gray-700 md:dark:hover:bg-transparent">What's on<svg class="w-2.5 h-2.5 ms-2.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
             <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 4 4 4-4"/>
           </svg></button>
           <div id="dropdownWhatsOn" class="z-10 hidden font-normal bg-white divide-y divide-gray-100 rounded-lg shadow-sm w-44 dark:bg-[rgb(32,32,80)] dark:divide-gray-600">
               <ul class="py-2 text-sm text-gray-700 dark:text-gray-200" aria-labelledby="dropdownLargeButton">
                 <li>
-                  <a href="/st-marys/sundays" class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Sundays</a>
+                  <a href="/st-marys/sundays" class="block px-4 py-2 md:hover:bg-transparent md:border-0 md:hover:text-blue-500">Sundays</a>
                 </li>
                 <li>
-                  <a href="/st-marys/midweek" class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Midweek</a>
+                  <a href="/st-marys/midweek" class="block px-4 py-2 md:hover:bg-transparent md:border-0 md:hover:text-blue-500">Midweek</a>
                 </li>
                 <li>
-                  <a href="/st-marys/students" class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Students</a>
+                  <a href="/st-marys/students" class="block px-4 py-2 md:hover:bg-transparent md:border-0 md:hover:text-blue-500">Students</a>
                 </li>
                 <li>
-                  <a href="/st-marys/children" class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Children</a>
+                  <a href="/st-marys/children" class="block px-4 py-2 md:hover:bg-transparent md:border-0 md:hover:text-blue-500">Children</a>
                 </li>
-                <li>
-                  <a href="/st-marys/events" class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Events</a>
-                </li>
+                <li aria-labelledby="dropdownNavbarLink">
+                    <button id="doubleDropdownButton" data-dropdown-toggle="doubleDropdown" data-dropdown-placement="right-start" type="button" class="flex items-center justify-between w-full px-4 py-2 md:hover:bg-transparent md:border-0 md:hover:text-blue-500">Events<svg class="w-2.5 h-2.5 ms-2.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
+                      <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 4 4 4-4"/>
+                      </svg>
+                    </button>
+                    <div id="doubleDropdown" class="z-10 hidden bg-white divide-y divide-gray-100 rounded-lg shadow-sm w-40 dark:bg-[rgb(32,32,80)]">
+                      <ul class="py-2 text-sm text-gray-700 dark:text-gray-200" aria-labelledby="doubleDropdownButton">
+                        <li>
+                          <a href="/st-marys/events/manual" class="block px-4 py-2 md:hover:bg-transparent md:border-0 md:hover:text-blue-500">Manual</a>
+                        </li>
+                        <li>
+                          <a href="/st-marys/events/instagram" class="block px-4 py-2 md:hover:bg-transparent md:border-0 md:hover:text-blue-500">Instagram</a>
+                        </li>
+                        <li>
+                          <a href="/st-marys/events/churchsuite" class="block px-4 py-2 md:hover:bg-transparent md:border-0 md:hover:text-blue-500">ChurchSuite</a>
+                        </li>
+                      </ul>
+                    </div>
+                  </li>
               </ul>
           </div>
         </li>
         <li>
-          <button id="dropdownLifeEventsLink" data-dropdown-toggle="dropdownLifeEvents" class="flex items-center justify-between w-full py-2 px-3 text-gray-900 hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 md:w-auto dark:text-white md:dark:hover:text-blue-500 dark:focus:text-white dark:hover:bg-gray-700 md:dark:hover:bg-transparent">Life Events<svg class="w-2.5 h-2.5 ms-2.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
+          <button id="dropdownLifeEventsLink" data-dropdown-toggle="dropdownLifeEvents" class="flex items-center justify-between w-full py-2 px-3 text-gray-900 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 md:w-auto dark:text-white md:dark:hover:text-blue-500 dark:focus:text-white dark:hover:bg-gray-700 md:dark:hover:bg-transparent">Life Events<svg class="w-2.5 h-2.5 ms-2.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
             <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 4 4 4-4"/>
           </svg></button>
           <div id="dropdownLifeEvents" class="z-10 hidden font-normal bg-white divide-y divide-gray-100 rounded-lg shadow-sm w-44 px-2 py-2 dark:bg-[rgb(32,32,80)] dark:divide-gray-600">
               <ul class="py-2 text-sm text-gray-700 dark:text-gray-200" aria-labelledby="dropdownLargeButton">
                 <li>
-                  <a href="/st-marys/christenings" class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Christenings</a>
+                  <a href="/st-marys/christenings" class="block px-4 py-2 md:hover:bg-transparent md:border-0 md:hover:text-blue-500">Christenings</a>
                 </li>
                 <li>
-                  <a href="/st-marys/baptisms" class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Baptisms</a>
+                  <a href="/st-marys/baptisms" class="block px-4 py-2 md:hover:bg-transparent md:border-0 md:hover:text-blue-500">Baptisms</a>
                 </li>
                 <li>
-                  <a href="/st-marys/weddings" class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Weddings</a>
+                  <a href="/st-marys/weddings" class="block px-4 py-2 md:hover:bg-transparent md:border-0 md:hover:text-blue-500">Weddings</a>
                 </li>
                 <li>
-                  <a href="/st-marys/funerals" class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Funerals</a>
+                  <a href="/st-marys/funerals" class="block px-4 py-2 md:hover:bg-transparent md:border-0 md:hover:text-blue-500">Funerals</a>
                 </li>
               </ul>
           </div>
         </li>
         <li>
-            <a href="/st-marys/bookings" class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 px-2 py-2 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Bookings</a>
+            <a href="/st-marys/bookings" class="block py-2 px-3 text-gray-900 rounded md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 px-2 py-2 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Bookings</a>
         </li>
         <li>
-          <a href="/st-marys/giving" class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 px-2 py-2 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Giving</a>
+          <a href="/st-marys/giving" class="block py-2 px-3 text-gray-900 rounded md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 px-2 py-2 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Giving</a>
         </li>
         <li>
           <a href="/st-marys/contact-us" 
