e80fcdf1d8eb51cdc6c6a60f5781344f410fe896
bahanley <benhanley321@gmail.com>
2025-08-26 22:27:53 +0100

    button update

--- src/views/Shared/Header.php
+++ src/views/Shared/Header.php
@@ -103,7 +103,7 @@
         </li>
         <li>
           <a href="/st-marys/contact-us" 
-            class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-2 py-2 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+            class="text-white bg-blue-700 hover:bg-indigo-500 focus:ring-4 focus:outline-none focus:ring-indigo-500 font-medium rounded-lg text-sm px-2 py-2 text-center dark:bg-blue-600 dark:hover:bg-indigo-500 dark:focus:ring-indigo-500">
             Contact us
           </a>
         </li>
